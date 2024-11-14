ecbb3f3699e36cfafa7304b0989382b9a744f75e
Romanov Michael <romanov.players@gmail.com>
2024-11-14 11:51:00 +0300

    add logic

---
+++ script.js
@@ -0,0 +1,53 @@
+const bindElements = document.querySelectorAll('[data-bind]')
+const bindingEnabled = document.querySelector('#bindingEnabled')
+const bindMap = {}
+const scope = {}
+
+for(const el of bindElements) {
+    const bindTo = el.dataset.bind
+
+    if(!bindMap[bindTo]) {
+        bindMap[bindTo] = []
+    }
+    bindMap[bindTo].push(el)
+    console.log(bindMap)
+}
+
+function bindValue(bindingName, value) {
+    scope[bindingName] = value
+}
+
+function syncBindings(target) {
+    for (const bindTo in scope) {
+        const value = scope[bindTo]
+
+        for (const el of bindMap[bindTo]) {
+            if(el !== target) {
+                if(el.tagName === 'INPUT') {
+                    el.value = value
+                } else {
+                    el.textContent = value
+                }
+            }
+        }   
+        delete scope[bindTo]
+    }
+}
+
+document.addEventListener('input', e => {
+    const target = e.target
+    const bindTo = target.dataset.bind
+    if(bindTo && bindMap[bindTo]) {
+        bindValue(bindTo, target.value)
+
+        if(bindingEnabled.checked) {
+            syncBindings(target)
+        }
+    }
+})
+
+bindingEnabled.addEventListener('change', () => {
+    if(bindingEnabled.checked) {
+        syncBindings()
+    }
+})
